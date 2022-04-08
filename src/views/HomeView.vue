beac1b65506dd6583658293f2271de7eae8aface
TsuZAke26 <ricky.sanders62@gmail.com>
2022-04-08 15:48:09 -0500

    Options API Example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,39 @@
 <template>
-  <div class="home"><h1>Home</h1></div>
+  <div class="home">
+    <div>
+      <button class="btn" @click="modifyCounter(-1)">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button class="btn" @click="modifyCounter(1)">+</button>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    modifyCounter(value: number) {
+      this.counter += value;
+    },
+  },
+});
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>

a0c884b474ec894e3a5c42b2aca27c401dddc0b7
TsuZAke26 <ricky.sanders62@gmail.com>
2022-04-09 12:07:37 -0500

    Child components & close modal with emit

---
+++ src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1><slot name="title" /></h1>
+      <h2>{{ title }}</h2>
+      <slot />
+      <button @click="handleHideModal">Hide Modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["show-modal"]);
+const handleHideModal = () => {
+  emit("show-modal", false);
+};
+</script>
+
+<style scoped>
+.modal {
+  background: blue;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>

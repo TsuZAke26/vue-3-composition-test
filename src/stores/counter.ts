582d27f57bce6318ab586e9aecbc5c72efefe5fd
TsuZAke26 <ricky.sanders62@gmail.com>
2022-04-10 08:18:53 -0500

    Using state menegement to control data in app

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,14 +3,17 @@ import { defineStore } from 'pinia';
 export const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0,
+    count: 0,
+    title: 'My Counter Title',
   }),
   getters: {
-    doubleCount: (state) => state.counter * 2,
+    oddOrEven: (state) => {
+      return state.count % 2 === 1 ? 'odd' : 'even';
+    },
   },
   actions: {
-    increment() {
-      this.counter++;
+    modifyCount(value: number) {
+      this.count += value;
     },
   },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="home">
     <h1 ref="appTitleRef">{{ appTitle }}</h1>
-    <div>My Counter: {{ counterData.title }}</div>
+    <div>{{ counter.title }}</div>
 
     <div>
       <button class="btn" @click="modifyCounter(-1)">-</button>
-      <span class="counter">{{ counterData.count }}</span>
+      <span class="counter">{{ counter.count }}</span>
       <button class="btn" @click="modifyCounter(1)">+</button>
     </div>
 
+    <div>Current value is {{ counter.oddOrEven }}</div>
+
     <div class="edit">
       <h4>Edit Counter Title:</h4>
-      <input v-model="counterData.title" type="text" v-autofocus />
+      <input v-model="counter.title" type="text" v-autofocus />
     </div>
   </div>
 </template>
 
 <!-- Composition API (script setup) -->
 <script setup lang="ts">
-import { reactive, ref, onMounted, nextTick } from "vue";
+import { ref, onMounted, nextTick } from "vue";
 import type { Ref } from "vue";
-
+import { useCounterStore } from "@/stores/counter";
 import { vAutofocus } from "@/directives/vAutofocus";
 
 const appTitle = ref("My Basic Counter App");
@@ -29,12 +31,9 @@ onMounted(() => {
   console.log(`The app title is ${appTitleRef.value?.offsetWidth} pixels wide`);
 });
 
-const counterData = reactive({
-  count: 0,
-  title: "",
-});
+const counter = useCounterStore();
 const modifyCounter = (value: number) => {
-  counterData.count += value;
+  counter.modifyCount(value);
   nextTick(() => {
     console.log("Counter has updated in DOM");
   });
